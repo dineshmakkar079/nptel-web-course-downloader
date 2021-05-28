18464ecb66527e2f02da24c1b21ae8aac2d12256
Dinesh Makkar <dineshmakkar079@gmail.com>
2021-05-28 21:45:02 +0530

    Fixed 404 / download errors
    1. Moved to requests from urllib library
    2. Added SSL fix
    3. Updated Base URL of file download
    4. http to https
    5. Added function to get extension from download URL.

--- nptel_pdf_web_course_downloader.py
+++ nptel_pdf_web_course_downloader.py
@@ -1,31 +1,38 @@
-import urllib.request
-#import urllib2
+import requests
 from bs4 import BeautifulSoup
 import os
 import sys
 from random import randint
 import time
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
+def getExtensionFromDownloadUrl (url = "") :
+	if (len(url) == 0):
+		return ""
+	ext = url.split('.')[-1] # at the end after a .
+	return "." + ext;
+
 if __name__ == '__main__':
 	startTime = time.time()
 	url = sys.argv[1]
-	url = 'http://' + url[ url.find('nptel') : ]
+	url = 'https://' + url[ url.find('nptel') : ]
 	courseId = url.split('/')[4]
 	title = ""
 	topicList = []
-	req = urllib.request.Request(url)
-	with urllib.request.urlopen(req) as response:
-		html = response.read()
+	with requests.get(url) as response:
+		html = response.content
 		soup = BeautifulSoup(html, 'lxml')
 		title = soup.find('ul', id='breadcrumbs-course').findAll('li')[2].text
 		title = title[ : title.find('(') ]
 		topicList = [row.text for row in soup.find('div', id='div_lm').findAll('li') if len(row.findChildren()) == 1]
 
 	fileNumber = 1
-	BASE_URL = 'http://nptel.ac.in/courses/' + courseId + '/'
+	BASE_URL = 'https://nptel.ac.in/courses/' + courseId + '/'
 	os.mkdir(title)
 	prevDirectory = os.getcwd()
 	os.chdir(title)
@@ -36,22 +43,24 @@ if __name__ == '__main__':
 	print("Files Downloaded : \n#0000")
 	while True : 
 		visitUrl = BASE_URL + str(fileNumber)
-		req = urllib.request.Request(visitUrl)
-		with urllib.request.urlopen(req) as response:
-			html = response.read()
+		with requests.get(visitUrl) as response:
+			html = response.content
 			soup = BeautifulSoup(html, 'lxml')
 			iframeElement = soup.find('iframe')
 			if iframeElement == None :
 				break
 			downloadUrlPart = str(iframeElement['src'])
 			fileNumberName = str(1000 + fileNumber)[1:] + '. '
-			downloadUrl = 'http://nptel.ac.in/courses/' + downloadUrlPart[ downloadUrlPart.find('/') + 1 : ]
+			downloadUrl = 'https://nptel.ac.in/' + downloadUrlPart[ downloadUrlPart.find('/') + 1 : ]
 			downloadUrl = "%20".join(downloadUrl.split(' '))
 			saveFileName = fileNumberName + topicList[fileNumber-1]
 			saveFileName = saveFileName.replace("/", "")
-			try :	
-				urllib.request.urlretrieve( downloadUrl , saveFileName )
-				#print("Downloaded : " + saveFileName)
+			try :
+				with requests.get(downloadUrl) as fileData:
+					extension = getExtensionFromDownloadUrl(downloadUrl)
+					filePointer = open(saveFileName+extension, 'wb')
+					filePointer.write(fileData.content)
+					filePointer.close()
 				totalFilesDownloaded = totalFilesDownloaded + 1
 				print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
 				print("#" + str(10000 + totalFilesDownloaded)[1:] + " : " + saveFileName)
@@ -60,6 +69,8 @@ if __name__ == '__main__':
 				notDownloadedUrls.append(downloadUrl)
 				print("Can't download file : " + saveFileName)
 		fileNumber = fileNumber + 1
+		if (fileNumber > len(topicList)):
+			break
 	os.chdir(prevDirectory)
 	endTime = time.time()
 	if(len(notDownloadedNumbers) == 0):
